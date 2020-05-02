0c7a2b58637b60584183d29187ffcbae1d2cfc74
Mukheem Mohammed <49473863+Mukheem1603@users.noreply.github.com>
2020-05-02 17:12:08 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,24 @@
+const toBeBlocked = [
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+    "*://*.zedo.com/*",
+    "*://*.nbc.com/*",
+    "*://*.imarticus.org/*",
+    "*://*.hostinger.in/*",
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) { return { cancel: true }},
+    { urls: toBeBlocked },
+    ["blocking"]
+)
+
+//you can do your research and add some more advertising services to 'toBeBlocked'
+//note: to access these web sites you need to disable the extension as the requests are blocked by our extension
